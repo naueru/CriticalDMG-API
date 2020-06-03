963e9718eea40f67c26bc5d05bd85f9509febac7
Lucas Botteri <lucasbotteri@hotmail.com>
2020-06-02 23:21:56 -0300

    Handling application specific errors in user creation
    In case a user already exists an internalCode is sent to facilitate error handling in client
    This error codes are listed in constants

--- src/services/users/users.hooks.ts
+++ src/services/users/users.hooks.ts
@@ -1,9 +1,23 @@
 import * as feathersAuthentication from "@feathersjs/authentication";
 import * as local from "@feathersjs/authentication-local";
+import { UserDTO } from "./users.dto";
+import { Hook } from "@feathersjs/feathers";
+import { ERROR_CODE } from "../../constants";
+const { FeathersError } = require("@feathersjs/errors");
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = feathersAuthentication.hooks;
 const { hashPassword, protect } = local.hooks;
+
+const handleCreationError: Hook<UserDTO> = (context) => {
+  const notUniqueError = context.error.errors.find((e: any) => e.validatorKey === "not_unique");
+  if (notUniqueError) {
+    const isMailNotUnique = notUniqueError.path === "email";
+    const internalError = isMailNotUnique ? ERROR_CODE.MAIL_ALREADY_IN_USE : ERROR_CODE.USERNAME_ALREADY_IN_USE;
+    context.error.data.internalError = internalError;
+  }
+  return context;
+};
 
 export default {
   before: {
@@ -15,30 +29,30 @@ export default {
     create: [hashPassword("password")],
     update: [hashPassword("password"), authenticate("jwt")],
     patch: [hashPassword("password"), authenticate("jwt")],
-    remove: [authenticate("jwt")]
+    remove: [authenticate("jwt")],
   },
 
   after: {
     all: [
       // Make sure the password field is never sent to the client
       // Always must be the last hook
-      protect("password")
+      protect("password"),
     ],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    create: [handleCreationError],
+    update: [handleCreationError],
+    patch: [handleCreationError],
+    remove: [],
+  },
 };
